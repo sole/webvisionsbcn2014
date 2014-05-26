34477f89d5339d5771bea064e214fcf8e67d627c
sole <sole@mozilla.com>
2014-05-26 18:01:47 +0100

    initial magic

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+	console.log('load');
+	var ta = document.querySelector('textarea');
+	var codeValue = trimInitialTabs(ta.value);
+	var cm = CodeMirror(function(el) {
+			ta.parentNode.replaceChild(el, ta);
+		}, {
+			value: codeValue
+		}
+	);
+
+	// MAGIC!
+	function trimInitialTabs(str) {
+		var tabsRe = /(\t*)/;
+		var tabsMatches = tabsRe.exec(str);
+		var numInitialTabs = 0;
+		if(tabsMatches && tabsMatches[1]) {
+			numInitialTabs = tabsMatches[1].length;
+		}
+		var replacementRe = new RegExp('^\t{' + numInitialTabs + '}');
+		var lines = str.split('\n').map(function(line) {
+			return line.replace(replacementRe, '');
+		});
+		return lines.join('\n');
+	}
+};
+
