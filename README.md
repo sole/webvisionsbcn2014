5485c932d4e936ca9415b9ce8c19aebe693c85c5
sole <sole@mozilla.com>
2014-06-02 20:54:51 -0500

    aaah, the importance of being prototypiest (literary bad puns!)

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 - active line background change with transition
 - load script in editor -> custom component? e.g. <x-editor src="example/amazing.js"></x-editor>
 - why does the web component not work in Firefox?
-- also say webcomponentsenabled needs to be true
+	- also say webcomponentsenabled needs to be true
+	- use <template>?
+	- is it possible to place the code mirror dependency inside the component only?
--- js/x-editor.js
+++ js/x-editor.js
@@ -1,35 +1,74 @@
 (function() {
-	// template with the textarea
-	// src attribute, xmlhttprequest
-	var proto = {
-		createdCallback: function() {
-			console.log('oh hey');
-			this.innerHTML = 'wiii';
-			var codeSrc = this.attributes.src.value;
-			// this.onCodeLoaded('aaaa');
-			if(codeSrc === undefined) {
-				this.onCodeLoaded('// No src specified');
-			} else {
-				this.loadCode(codeSrc);
-			}
-		},
-		loadCode: function(url) {
-			var request = new XMLHttpRequest();
-			var that = this;
-			console.log('x-editor: loading script', url);
-			request.open('get', url, true);
-			request.responseType = 'text';
-			request.onload = function() {
-				that.onCodeLoaded(request.response);
-			};
-			request.onerror = function() {
-				that.onCodeLoaded('// ERROR loading ' + url);
-			};
-			request.send();
-		},
-		onCodeLoaded: function(code) {
-			this.innerHTML = '<textarea>' + code + '</textarea>';
+
+	// MAGIC!
+	function trimInitialTabs(str) {
+		var tabsRe = /(\t*)/;
+		var tabsMatches = tabsRe.exec(str);
+		var numInitialTabs = 0;
+		if(tabsMatches && tabsMatches[1]) {
+			numInitialTabs = tabsMatches[1].length;
 		}
+		var replacementRe = new RegExp('^\t{' + numInitialTabs + '}');
+		var lines = str.split('\n').map(function(line) {
+			return line.replace(replacementRe, '');
+		});
+		return lines.join('\n');
+	}
+
+	var runCode = (function makeEval() {
+		var cheatyEval = eval;
+		return function (str) {
+			cheatyEval(str);
+		};
+	})();
+
+
+	var proto = Object.create(HTMLElement.prototype);
+
+	proto.createdCallback = function() {
+		console.log('oh hey');
+		this.innerHTML = 'wiii';
+		var codeSrc = this.attributes.src.value;
+		
+		if(codeSrc === undefined) {
+			this.onCodeLoaded('// No src specified');
+		} else {
+			this.loadCode(codeSrc);
+		}
+	};
+
+	proto.loadCode = function(url) {
+		var request = new XMLHttpRequest();
+		var that = this;
+		console.log('x-editor: loading script', url);
+		request.open('get', url, true);
+		request.responseType = 'text';
+		request.onload = function() {
+			that.onCodeLoaded(request.response);
+		};
+		request.onerror = function() {
+			that.onCodeLoaded('// ERROR loading ' + url);
+		};
+		request.send();
+	};
+
+	proto.onCodeLoaded = function(code) {
+		var that = this;
+		var ta = document.createElement('textarea');
+		this.appendChild(ta);
+		
+		var codeValue = trimInitialTabs(code).trimRight();
+		var cm = CodeMirror(function(el) {
+				that.replaceChild(el, ta);
+			}, {
+				value: codeValue,
+				lineWrapping: true,
+				lineNumbers: true,
+				styleActiveLine: true,
+				matchBrackets: true,
+				showTrailingSpace: true,
+			}
+		);
 	};
 
 	document.registerElement('x-editor', {
