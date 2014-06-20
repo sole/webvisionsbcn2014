cfd451225b1ce41a62cf62d20805573630b26ed0
sole <sole@mozilla.com>
2014-06-21 00:56:04 +0200

    + couple commented things on oscillator example

--- src/examples/01_oscillator/main.js
+++ src/examples/01_oscillator/main.js
@@ -4,3 +4,8 @@ oscillator.connect(audioContext.destination);
 oscillator.start();
 oscillator.frequency.value = 220;
 oscillator.frequency.linearRampToValueAtTime(440, audioContext.currentTime + 2);
+//oscillator.type = 'sine'
+/*setTimeout(function() {
+  oscillator.frequency.linearRampToValueAtTime(Math.random() * 100 + 100, audioContext.currentTime + 0.25);
+}, 300);
+*/
