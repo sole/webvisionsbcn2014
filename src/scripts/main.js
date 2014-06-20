268997f268d23631b787419acf1cba5d242ccf11
sole <sole@mozilla.com>
2014-06-21 00:15:43 +0200

    add fullscreen but this should be in a plugin

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,37 @@ bespoke.from('article', {
   convenient: true,
   secondary: true
 });
+
+// Add contextual menu. HTML5 is the <3
+var menu = document.createElement('menu');
+var item = document.createElement('menuitem');
+menu.setAttribute('id', 'fsmenu');
+menu.setAttribute('type', 'context');
+item.setAttribute('label', 'Fullscreen');
+item.addEventListener('click', toggleFullScreen);
+menu.appendChild(item);
+document.body.appendChild(menu);
+document.body.setAttribute('contextmenu', 'fsmenu');
+
+function toggleFullScreen() {
+
+	var requestFS = document.body.requestFullScreen ||
+		document.body.mozRequestFullScreen ||
+		document.body.webkitRequestFullScreen;
+
+	var cancelFS = document.body.cancelFullScreen ||
+		document.mozCancelFullScreen ||
+		document.webkitCancelFullScreen;
+
+	var fs = window.fullScreen ||
+		document.fullscreenElement ||
+		document.mozFullScreenElement ||
+		document.webkitFullscreenElement;
+
+	if(fs) {
+		cancelFS.call(document);
+	} else {
+		requestFS.call(document.body, Element.ALLOW_KEYBOARD_INPUT);
+	}
+
+}
