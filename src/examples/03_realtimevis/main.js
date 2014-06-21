463c6fef225272bef60075daf41b3e3b716d8d0f
sole <sole@mozilla.com>
2014-06-21 12:38:03 +0200

    well...

--- src/examples/03_realtimevis/main.js
+++ src/examples/03_realtimevis/main.js
@@ -10,6 +10,7 @@ var analyser = audioContext.createAnalyser();
 var analyserData;
 analyser.connect(audioContext.destination);
 analyser.fftSize = 2048;
+analyser.smoothingTimeConstant = 0.5;
 analyserData = new Uint8Array(analyser.frequencyBinCount);
 
 
@@ -17,7 +18,7 @@ analyserData = new Uint8Array(analyser.frequencyBinCount);
 // 3D setup
 var sceneWidth = 320;
 var sceneHeight = 240;
-var range = 30;
+var range = 250;
 
 var content = document.getElementById('content');
 var renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -36,7 +37,7 @@ light.castShadow = true;
 //light.shadowCameraVisible = true;
 light.shadowMapWidth = 1024;
 light.shadowMapHeight = 1024;
-var d = 200;
+var d = 400;
 light.shadowCameraLeft = -d;
 light.shadowCameraRight = d;
 light.shadowCameraTop = d;
@@ -46,7 +47,7 @@ light.shadowCameraFar = 1000;
 light.shadowDarkness = 0.5;
 scene.add(light);
 
-var planeGeometry = new THREE.PlaneGeometry(500, 500);
+var planeGeometry = new THREE.PlaneGeometry(1000, 1000);
 var planeMaterial = new THREE.MeshPhongMaterial({ color: 0xFF0000 });
 //var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xFF00FF, wireframe: true });
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
@@ -65,17 +66,15 @@ var visualScale = 20;
 // TODO white on red background, +shadows
 content.appendChild( renderer.domElement );
 renderer.setSize(sceneWidth, sceneHeight);
-renderer.setClearColor( 0xffffff, 1.0 );
+renderer.setClearColor( 0xff0000, 1.0 );
 
-var camera = new THREE.PerspectiveCamera( 45, sceneWidth / sceneHeight, 1, 100000 );
+var camera = new THREE.PerspectiveCamera( 90, sceneWidth / sceneHeight, 1, 100000 );
 var cameraTarget = new THREE.Vector3( 0, 0, 0 );
-//camera.position.set( 0, 10, range );
-camera.position.set( 0, 10, 10 );
 var r = 300;
-camera.position.set( 0, r, r );
+camera.position.set(0, range, range);
 camera.lookAt( cameraTarget );
 
-var left = -u * numBars;
+var left = -u * numBars + u;
 var x = left;
 for(var i = 0; i < numBars; i++) {
   var bar = new THREE.Mesh(geometry, material);
@@ -103,29 +102,32 @@ navigator.getUserMedia(
 function animate(timestamp) {
   requestAnimationFrame(animate);
   
-  analyser.getByteTimeDomainData(analyserData); // 0..255
+  analyser.getByteFrequencyData(analyserData); // 0..255
 
   updateVisualisation(analyserData);
 
-  var t = timestamp !== null ? timestamp * 0.001 : 0;
-//  camera.position.set(0, range * Math.sin(t), range * Math.cos(t));
-//  camera.lookAt(cameraTarget);
+  var t = timestamp !== null ? timestamp * 0.0002 : 0;
+  //camera.position.set(range * Math.sin(t), range / 2, range * Math.cos(t));
+  //camera.lookAt(cameraTarget);
 
-  //light.position.set(0, range* 0.5 + range *0.25* Math.sin(t), 0);
   renderer.render(scene, camera);
   
 }
 
 function updateVisualisation(data) {
-  var dataLength = data.length;
+
+  var dataLength = data.length / 4;
   var dataIndex = 0;
   var skipLength = Math.round(dataLength / numBars);
 
-  for(var j = 0; j < numBars; j++) {  
-    var v = data[dataIndex] / 255.0;
+  for(var j = 0; j < numBars; j++) {
+    
+    var v =  data[dataIndex] / 255.0;
     dataIndex += skipLength;
-    row[j].scale.set(1, 1, v * visualScale);
+    var bar = row[j];
+    bar.scale.set(1, 1, 1 + v * visualScale);
   }
+
 }
 
 // GO GO GO GO
