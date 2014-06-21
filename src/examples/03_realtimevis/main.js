23c4f4d0f1ca2b2d5770ee1f9a62466b3a707c52
sole <sole@mozilla.com>
2014-06-21 10:47:00 +0200

    getting there!

--- src/examples/03_realtimevis/main.js
+++ src/examples/03_realtimevis/main.js
@@ -15,13 +15,25 @@ analyserData = new Uint8Array(analyser.frequencyBinCount);
 
 
 // 3D setup
+var sceneWidth = 320;
+var sceneHeight = 240;
+var range = 20;
 var content = document.getElementById('content');
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 var scene = new THREE.Scene();
+var geometry = new THREE.BoxGeometry(5, 5, 5);
+var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
+var cube = new THREE.Mesh(geometry, material);
 
 content.appendChild( renderer.domElement );
-renderer.setSize(320, 240);
+renderer.setSize(sceneWidth, sceneHeight);
 renderer.setClearColorHex( 0xeeeeee, 1.0 );
+
+var camera = new THREE.PerspectiveCamera( 45, sceneWidth / sceneHeight, 1, 10000 );
+camera.position.set( 0, 0, range );
+camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+
+scene.add(cube);
 
 // Starting the audio stream
 navigator.getUserMedia(
@@ -40,6 +52,12 @@ function animate() {
   requestAnimationFrame(animate);
   
   analyser.getByteTimeDomainData(analyserData); // 0..255
+
+  var v = analyserData[0] / 255.0;
+
+  cube.scale.set(v, v, v);
+
+  renderer.render(scene, camera);
 
   
 }
