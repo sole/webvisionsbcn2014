c9c5ac538d5b22340b6dc4e9dc472e51ff499e56
sole <sole@mozilla.com>
2014-06-21 11:09:50 +0200

    maaaaybe I still know how to 3D

--- src/examples/03_realtimevis/main.js
+++ src/examples/03_realtimevis/main.js
@@ -18,22 +18,35 @@ analyserData = new Uint8Array(analyser.frequencyBinCount);
 var sceneWidth = 320;
 var sceneHeight = 240;
 var range = 20;
+var u = 1;
 var content = document.getElementById('content');
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 var scene = new THREE.Scene();
-var geometry = new THREE.BoxGeometry(5, 5, 5);
-var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
-var cube = new THREE.Mesh(geometry, material);
+var geometry = new THREE.BoxGeometry(u, u, u);
+var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true, wireframeLinewidth: 3 });
+var numBars = 20;
+var row = [];
+var visualScale = 20;
 
+// TODO white on red background, +shadows
 content.appendChild( renderer.domElement );
 renderer.setSize(sceneWidth, sceneHeight);
-renderer.setClearColorHex( 0xeeeeee, 1.0 );
+renderer.setClearColor( 0xff0000, 1.0 );
 
 var camera = new THREE.PerspectiveCamera( 45, sceneWidth / sceneHeight, 1, 10000 );
-camera.position.set( 0, 0, range );
-camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+var cameraTarget = new THREE.Vector3( 0, 0, 0 );
+camera.position.set( 0, range / 2, range );
+camera.lookAt( cameraTarget );
 
-scene.add(cube);
+var left = -u * numBars;
+var x = left;
+for(var i = 0; i < numBars; i++) {
+  var bar = new THREE.Mesh(geometry, material);
+  bar.position.set(x, 0, 0);
+  scene.add(bar);
+  row.push(bar);
+  x += u * 2;
+}
 
 // Starting the audio stream
 navigator.getUserMedia(
@@ -53,13 +66,23 @@ function animate() {
   
   analyser.getByteTimeDomainData(analyserData); // 0..255
 
-  var v = analyserData[0] / 255.0;
-
-  cube.scale.set(v, v, v);
+  updateVisualisation(analyserData);
 
   renderer.render(scene, camera);
 
   
+}
+
+function updateVisualisation(data) {
+  var dataLength = data.length;
+  var dataIndex = 0;
+  var skipLength = Math.round(dataLength / numBars);
+
+  for(var j = 0; j < numBars; j++) {  
+    var v = data[dataIndex] / 255.0;
+    dataIndex += skipLength;
+    row[j].scale.set(1, 1, v * visualScale);
+  }
 }
 
 // GO GO GO GO
